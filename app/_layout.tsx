caf05278e01919ff999aa2ad3caf18534cfaf109
Jonathan Velasquez <johedvelca@gmail.com>
2025-06-03 21:35:29 -0400

    log in still works 2.06.29

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,6 +3,8 @@ import { SplashScreen, Stack } from "expo-router";
 import { useEffect } from "react";
 import './globals.css';
 
+import GlobalProvider from '../context/GlobalProvider';
+
 SplashScreen.preventAutoHideAsync();
 
 const RootLayout = () => {
@@ -32,12 +34,14 @@ const RootLayout = () => {
   }
 
   return (
+    <GlobalProvider>
       <Stack>
         <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="(auth)" options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         {/* <Stack.Screen name="/search/[query]" options={{ headerShown: false }} /> */}
       </Stack>
+    </GlobalProvider>
   );
 }
 
