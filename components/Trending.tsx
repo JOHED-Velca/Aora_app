d43932d7c72674aaffa90a5c4a2773c0deb442db
Jonathan Velasquez <johedvelca@gmail.com>
2025-06-07 22:21:40 -0400

    VideoCard styled, doesnt get cut anymore. moving on to the next seccion.

--- components/Trending.tsx
+++ components/Trending.tsx
@@ -51,33 +51,22 @@ const TrendingItem = ({activeItem, item}:any) => {
   
   return (
     <Animatable.View
-      className='mr-5'
+      className='ml-5 mb-6'
       animation={activeItem === item.$id ? zoomIn : zoomOut}
       duration={500}
     >
-      <View className='w-52 h-72 rounded-[35px] mt-3 bg-white/10 overflow-hidden'>
+      <View className='w-52 h-72 rounded-[35px] mt-3 bg-primary overflow-hidden'>
         {play ? (
           <VideoView
             player={player}
-            contentFit='contain'
+            contentFit='cover'
+            allowsFullscreen
             nativeControls
             style={{
               width: '100%',
               height: '100%',
             }}
           />
-          // <Video
-          //   source={{ uri: item.video }}
-          //   className='w-52 h-72 rounded-[35px] mt-3 bg-white/10'
-          //   resizeMode='contain'
-          //   useNativeControls
-          //   shouldPlay
-          //   onPlaybackStatusUpdate={(status:any) => {
-          //     if('didJustFinish' in status && status.didJustFinish) {
-          //       setPlay(false);
-          //     }
-          //   }}
-          // />
         ) : (
           <TouchableOpacity
             className='relative justify-center items-center'
