d38beff95a7f738e5ca6764131e2480d56c448b1
Jonathan Velasquez <johedvelca@gmail.com>
2025-06-08 20:33:38 -0400

    Search is not working, had to add a new index of type fulltext in Appwrite.

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -1,21 +1,34 @@
 import React, { useState } from 'react'
-import { Image, TextInput, TouchableOpacity, View } from 'react-native'
+import { Alert, Image, TextInput, TouchableOpacity, View } from 'react-native'
 
 import { icons } from '@/constants'
+import { router, usePathname } from 'expo-router'
 
-const SearchInput = ({ title, value, placeholder, handleChangeText, otherStyles, ...props }: any) => {
-    const[showPassword, setShowPassword] = useState(false)
+const SearchInput = ({ initialQuery }: any) => {
+  const pathname = usePathname()
+  const [query, setQuery] = useState(initialQuery || '')
+    
   return (
     <View className="w-full h-16 px-4 bg-black-100 rounded-2xl border-2 border-black-200 focus:border-secondary flex flex-row items-center space-x-4">
     <TextInput
         className="text-base mt-0.5 text-white flex-1 font-pregular"
-        value={value}
+        value={query}
         placeholder="Search for a video topic"
-        placeholderTextColor="#7B7B8B"
-        onChangeText={handleChangeText}
-        secureTextEntry={title === "Password" && !showPassword}
+        placeholderTextColor="#CDCDE0"
+        onChangeText={(e) => setQuery(e)}
     />
-    <TouchableOpacity>
+    <TouchableOpacity
+      onPress={() => {
+        if(!query) {
+          return Alert.alert('Missing query', 'Please enter a search term.')
+        }
+
+        if(pathname.startsWith('/search'))router.setParams({query})// If already on search page, just update the query
+        else router.push(`/search/${query}`)// Navigate to search page with the query
+          
+          
+      }}
+    >
         <Image
             source={icons.search}
             className="w-5 h-5"
